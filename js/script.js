57ae4e54bcce291c8a26c2f3344a2e1ff8d82fb5
salzanopm <salzanopm@email.com>
2021-11-11 16:46:04 +0100

    finish

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// esercizio di oggi: (insicurissimo) password generator
+// nome repo: js-pwdgen-wannabe
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+// nome utente
+const userName = prompt('Dimmi il tuo nome');
+// cognome utente
+const userSurname = prompt('Dimmi il tuo cognome');
+// colore preferito
+const userfavcolor = prompt('Dimmi il tuo colore preferito');
+// stampa nomecognomecolorepreferito21
+const userMessage = 'la tua nuova password è :' + userName + userSurname + userfavcolor + 21;
+
+// OUTPUT
+// Stampo il risultato all'utente
+alert(userMessage);
